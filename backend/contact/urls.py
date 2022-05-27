fd4f5bd0be79a637ce77e81faefa7573bd80488d
Robert Gessler <ragessler@Roberts-MBP.kc.rr.com>
2022-05-27 09:40:20 -0500

    chore: completed urls and views for contact

--- backend/contact/urls.py
+++ backend/contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from contact import views
 
 urlpatterns = [
-    path('', views.user_contacts)
+    path('', views.user_contacts),
+    path('<int:pk>/', views.contact_detail)
 ]
--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -7,7 +8,7 @@ from .serializers import ContactSerializer
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def user_contacts(request):
     if request.method == 'GET':
@@ -20,3 +21,23 @@ def user_contacts(request):
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'DELETE', 'PATCH'])
+@permission_classes([IsAuthenticated])
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'GET':
+        serializer = ContactSerializer(contact)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "DELETE":
+        serializer = ContactSerializer(contact)
+        contact.delete()
+        return_value = serializer.data
+        return_value['id'] = pk
+        return Response(return_value, status=status.HTTP_204_NO_CONTENT)
+    elif request.method == "PATCH":
+        serializer = ContactSerializer(contact, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
