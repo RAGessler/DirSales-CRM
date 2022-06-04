76fbc1879398446fd3756e9e75d4d38d93e210d6
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-06-04 16:05:47 -0500

    feature: added ability to mark a task as complete or delete a task

--- frontend/src/components/TaskList/TaskList.jsx
+++ frontend/src/components/TaskList/TaskList.jsx
@@ -1,8 +1,12 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import useAuth from '../../hooks/useAuth';
+import { URL_HOST } from '../../urlHost';
 import TaskUpdate from '../TaskUpdate/TaskUpdate';
 const TaskList = (props)=>{
+    const [user, token]=useAuth();
     const [displayTasks, setDisplayTasks]=useState(props.tasks)
     useEffect(()=>{
         setDisplayTasks(props.tasks)
@@ -10,12 +14,64 @@ const TaskList = (props)=>{
 
     const isComplete = (task)=>{
         if(task.complete===true){
-            return('Complete')
+            return('Completed')
         }
         else{
             return('Incomplete')
         }
     }
+
+    async function completeTask(task){
+        if(task.complete===false){
+            try{
+                let response = await axios.patch(`${URL_HOST}/api/tasks/task/${task.id}/`,{complete: true},{
+                    headers:{
+                        Authorization:'Bearer '+token
+                    }
+                })
+                if(response.status===202){
+                    props.getContactTasks(props.contact.id)
+                    alert('Success!')
+                }
+            }catch(error){
+                console.log(error.message)
+            }
+        }
+        else{
+            try{
+                let response = await axios.patch(`${URL_HOST}/api/tasks/task/${task.id}/`, {complete: false},{
+                    headers:{
+                        Authorization:'Bearer '+token
+                    }
+                })
+                if(response.status===202){
+                    props.getContactTasks(props.contact.id)
+                    alert('Success')
+                }
+            }catch(error){
+                console.log(error.message)
+            }
+        }
+    }
+    async function deleteTask(taskId){
+        try{
+            let response = await axios.delete(`${URL_HOST}/api/tasks/task/${taskId}/`,{
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            if (response.status === 204){
+                await props.getContactTasks(props.contact.id)
+                alert('Success!')
+            }
+        }
+        catch (error){
+            console.log(error.message)
+            alert(error.message)
+        }
+    }
+
+
     return(
         <div>
             {displayTasks.map((task, element)=>{
@@ -27,6 +83,8 @@ const TaskList = (props)=>{
                                 <Popup trigger={<button>Edit</button>} modal='true'>
                                     <TaskUpdate getContactTasks={props.getContactTasks} task={task} contact={props.contact}/>
                                 </Popup>
+                                <button onClick={()=>completeTask(task)}>Toggle Completion</button>
+                                <button onClick={()=>deleteTask(task.id)}>Delete</button>
                             </div>
                     </div>
                 )
